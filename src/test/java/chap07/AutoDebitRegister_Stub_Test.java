48bf09eec178fbc9e38572146bcbc3d139f540df
sungsanghyun <sos9215@gmail.com>
2022-09-27 23:34:55 +0900

    대역을 사용한 카드정보조회 테스트코드 예시

---
+++ src/test/java/chap07/AutoDebitRegister_Stub_Test.java
@@ -0,0 +1,40 @@
+package chap07;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static chap07.CardValidity.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AutoDebitRegister_Stub_Test {
+    private AutoDebitRegister register;
+    private StubCardNumberValidator stubValidator;
+    private StubAutoDebitInfRepsitory stubRepository;
+
+    @BeforeEach
+    void setUp() {
+        stubValidator = new StubCardNumberValidator();
+        stubRepository = new StubAutoDebitInfoRepository();
+        register = new AutoDebitRegister(stubValidator, stubRepository);
+    }
+
+    @Test
+    void invalidCard() {
+        stubValidator.setInvalidNo("111122223333");
+
+        AutoDebitReq req = new AutoDebitReq("user1", "111122223333");
+        RegisterResult result = this.register.register(req);
+
+        assertEquals(INVALID,result.getValidity());
+    }
+
+    @Test
+    void theftCard() {
+        stubValidator.setTheftNo("1234567890123456");
+
+        AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
+        RegisterResult result = this.register.register(req);
+
+        assertEquals(THEFT,result.getValidity());
+    }
+}
